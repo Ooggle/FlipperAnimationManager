34bc8e8af28e851c0647365f8c3728d8b5751842
SticksDev <tsommers150@gmail.com>
2022-12-27 12:51:26 -0500

    Update setupWinBuild.ps1

--- setupWinBuild.ps1
+++ setupWinBuild.ps1
@@ -7,10 +7,13 @@ $url = "https://github.com/libsdl-org/SDL/releases/download/release-2.26.1/SDL2-
 $zipFile = "SDL.zip"
 $targetDirectory = "tmp_sdl"
 
+# SDL Version
+$version = "2.26.1"
+
 # Download the SDL ZIP file
 Write-Output "Downloading SDL"
 $response = Invoke-WebRequest -Uri $url -OutFile $zipFile
-Write-Output "Done, server reply: $($response.StatusCode)"
+Write-Output "Done."
 
 # Extract the SDL file
 Write-Output "Extracting..."
@@ -18,15 +21,9 @@ Expand-Archive -Path $zipFile -DestinationPath $targetDirectory
 
 # Set the source and target directories for the copy operation 
 Write-Output "Moving .h files..."
-$sourceDirectory = "$targetDirectory\include"
+$sourceDirectory = "$targetDirectory\SDL2-$version\include"
 $targetDirectory = "."
 
 # Copy the contents of \include
 Get-ChildItem $sourceDirectory | Copy-Item -Destination $targetDirectory
-
-
-# Remove the tmp_sdl folder
-Write-Output "Removing tmp_sdl"
-Remove-Item -Recurse -Force $targetDirectory
-
 Write-Output "DONE! SDL is ready for the ci runner. Continuing...."
